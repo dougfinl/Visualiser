dedca5e44091f8926767aa075723a4ef0b155791
Douglas Finlay <douglas@douglasfinlay.com>
2016-12-29 15:49:15 +0000

    Initial Commit

---
+++ Visualiser/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Visualiser
+//
+//  Created by Douglas Finlay on 29/12/2016.
+//  Copyright © 2016 Douglas Finlay. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
